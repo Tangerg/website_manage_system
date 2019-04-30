cb9a2cf7ba361dcec4fbcc1243e13d7b3d3382a4
Tangerg <tangerg@qq.com>
2019-04-30 21:17:16 +0800

    修改跨域设置

---
+++ src/main/java/com/suse/netcenter/filter/CorsConf.java
@@ -0,0 +1,26 @@
+package com.suse.netcenter.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author Tangerg
+ * @create 2018-3-26 20:54
+ * 跨域配置
+ */
+@Configuration
+public class CorsConf {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
